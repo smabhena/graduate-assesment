2bc3ebf9d0ec083c5864f4e48bb7a9ba480f8304
Sino <100207221+smabhena@users.noreply.github.com>
2022-04-27 17:40:54 +0200

    Added functionality to view all saved locations

--- Weather App/Weather App/Features/Favourites/FavouriteViewController.swift
+++ Weather App/Weather App/Features/Favourites/FavouriteViewController.swift
@@ -8,22 +8,52 @@
 import UIKit
 
 class FavouriteViewController: UIViewController {
+    @IBOutlet private var favouritesTableView: UITableView!
+    
+    private lazy var viewModel = FavouriteViewModel(delegate: self,
+                                                    repository: FavouriteRepository())
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        setUpTableView()
+        title = "Favourites"
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    override func viewDidAppear(_ animated: Bool) {
+        viewModel.allSavedLocations()
     }
-    */
+    
+    func setUpTableView() {
+        self.favouritesTableView.delegate = self
+        self.favouritesTableView.dataSource = self
+    }
+}
+extension FavouriteViewController: FavouriteViewModelDelegate{
+    func reloadView() {
+        self.favouritesTableView.reloadData()
+    }
+    
+    func show(error: String) {
+        self.displayAlert(title: "Error",
+                          message: "Failed to fetch saved locations",
+                          buttonTitle: "Try again")
+    }
+}
 
+extension FavouriteViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return viewModel.locationsCount
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        
+        guard let location = viewModel.location(atIndex: indexPath.row) else {
+            return UITableViewCell()
+        }
+        
+        cell.textLabel?.text = location.location
+        
+        return cell
+    }
 }
